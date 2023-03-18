a5937eb85dbf57c5b4ec17d12789a94541c3e11b
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-03-18 21:14:12 +0500

    new files

---
+++ app.js
@@ -0,0 +1,92 @@
+    //Question no1
+    
+/*var inpt1=prompt('enter first name')
+var inpt2=prompt('enter last name')
+document.write('Full name'+' '+inpt1+inpt2)*/
+    //Question no2
+   
+/*var inpt=prompt('enter mobile name')
+document.write('Length of string :'+inpt.length)*/
+    //Question no 3
+    
+/*var str='Pakistani'
+var ind=str.indexOf('t');
+console.log(ind)*/
+    // Question no4
+    
+/*var str='Hello World'
+var ind=str.lastIndexOf('l');
+console.log(ind)*/
+    //Question no5
+
+/*var str='Pakistani'
+var ind=str.charAt(5);
+console.log(ind)*/
+    //Question no6
+    
+/*var inpt1=prompt('enter first name')
+var inpt2=prompt('enter last name')
+document.write(inpt1.concat(inpt2));*/
+    //Question no7
+    
+/*var str='Hyderabad'
+var fun=str.replace('Hyder','Islam');
+console.log(fun);*/
+    //Question no8
+    
+/*var message = 'Ali and Sami are best friends. They play cricket and  football together.'
+var rep=message.replaceAll('and','&');
+console.log(rep);*/
+
+    //Question no9
+/*var str='472'
+console.log(str)
+console.log(typeof(str))
+var str2=Number('472')
+console.log(str2)
+console.log(typeof(str2))
+var str3=+'472'
+console.log(str3)
+console.log(typeof(str3))*/
+    //Question no 10
+    
+/*var inpt=prompt('enter name');
+var str=inpt.toUpperCase();
+console.log(str)*/
+    //Question no11
+
+/*var user=prompt('enter name')
+for (var i=0; i<user.length;i++) {
+  console.log(user[i][0].toUpperCase()+user.slice(1))
+  break;
+}*/
+    //Question no12
+    
+/*var num =35.36;
+var chng=num.toString();
+console.log(chng);
+console.log(typeof(chng));*/
+    //Question no14
+    
+/*var item=['cake','biscuit','apple pie','patties','cookies']
+var inpt=prompt('enter item name').toLowerCase();
+var check= 'no';
+for (var i = 0; i <item.length; i++) {
+  if (inpt===item[i]) {
+    check='yes'
+alert('yes')
+      break;
+  }
+}
+if(check==='no'){
+    alert('no')}*/
+
+    //Question no 16
+    
+//var uni = 'University of Karachi'
+
+/*var uniArr=uni.split(',')
+console.log(uniArr);
+for (var i = 0; i < uni.length; i++) {
+  console.log(uni[i])
+}*/
